32d67856e3a882da5dbf1b7cd97c1943783abaf4
tygedavis <tdavis@cmsonline.com>
2020-07-27 12:22:08 -0600

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "tyge-serverless-tutorial"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ou0jdz5ly6.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_rKKy3Br0i",
+    APP_CLIENT_ID: "3necbqs4psu24i6ndhjqbpm34",
+    IDENTITY_POOL_ID: "us-east-1:f4bb4bb0-599a-4b74-8a0f-cf3a705bf41e"
+  }
+};
